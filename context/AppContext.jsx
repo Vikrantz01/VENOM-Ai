266f4be640d3a5222834fe65949ba309eae64fe9
Vikrant <163637775+Vikrantz01@users.noreply.github.com>
2026-02-13 21:44:10 +0530

    first commit

---
+++ context/AppContext.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { createContext, useContext } from "react";
+
+export const AppContext = createContext();
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+}
+
+export const AppProvider = ({ children}) => {
+    const {user} = useUser()
+    const value = {
+        user
+    }
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>
+}
